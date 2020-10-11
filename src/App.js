d413b9c24d13dd8527cceef33f04d73e4ad3467a
Rami41895 <ramiimar051@gmail.com>
2020-10-11 19:46:30 +0100

    My App relax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Audio from "./Component/video/sea-waves-sound-effect.mp3";
 
-export default App;
+function App ()  {
+
+  const initialMinute = 2
+  const initialSeconds = 0
+  const [ minutes, setMinutes ] = useState(initialMinute);
+  const [seconds, setSeconds ] =  useState(initialSeconds);
+
+  useEffect(()=>{
+    let myInterval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(myInterval)
+                } else {
+                    setMinutes(minutes - 1);
+                    setSeconds(59);
+                }
+            } 
+        }, 1000)
+        return ()=> {
+            clearInterval(myInterval);
+          };
+    });
+
+    return (
+      <div className="App">
+        <div>
+          <img src="/image/logo.png" alt="LOGO" />
+        </div>
+        <audio controls autoPlay loop >
+          <source className="audio" src={Audio} type="audio/ogg" />
+        </audio>
+        <div className="TITLE">
+          {" "}
+          <img src="/image/title.png" alt="title" />
+        </div>
+
+        <div className="logo">
+          <img src="/image/just-relax.png" alt="Relax" />
+        </div>
+        
+        <div className="Minuteur">
+        { minutes === 0 && seconds === 0
+            ? null
+            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+        }
+        </div>
+      </div>
+    );
+  }
+export default App
